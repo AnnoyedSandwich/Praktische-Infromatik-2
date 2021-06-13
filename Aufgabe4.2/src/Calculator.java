afaf8bdc070a83b55232430130bb1c181045b64d
AnnoyedSandwich <41872159+AnnoyedSandwich@users.noreply.github.com>
2021-06-13 23:57:38 +0200

    test
    test

--- Aufgabe4.2/src/Calculator.java
+++ Aufgabe4.2/src/Calculator.java
@@ -3,24 +3,34 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class Calculator implements ActionListener{
+
+    //textfelder, buttons
     JTextField input1, input2, output;
     JButton plusButton, minusButton;
+
+    //frame und panel
     JFrame frame;
     JPanel panel = new JPanel();
+
     Calculator(){
         frame = new JFrame();
+
         input1 = new JTextField( 16);
         input2 = new JTextField( 16);
         output = new JTextField("Ergebnis");
         output.setEditable(false);
+
         Font fo = new Font("Serif", Font.BOLD,20);
         output.setFont(fo);
+
         plusButton = new JButton("+");
         minusButton = new JButton("-");
         plusButton.addActionListener(this);
         minusButton.addActionListener(this);
+
         panel.add(input1);panel.add(input2);panel.add(output);panel.add(plusButton);panel.add(minusButton);
         frame.add(panel);
+
         frame.setSize(300,300);
         frame.setVisible(true);
     }
